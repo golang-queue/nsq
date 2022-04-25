b0ff8fdb7f08186bbe0c61f41f16cefa4c4adb3a
Bo-Yi Wu <appleboy.tw@gmail.com>
2022-04-25 17:21:03 +0800

    chore(worker): remove disable consumer flag (#16)

--- nsq.go
+++ nsq.go
@@ -19,10 +19,11 @@ var _ core.Worker = (*Worker)(nil)
 type Worker struct {
 	q         *nsq.Consumer
 	p         *nsq.Producer
+	cfg       *nsq.Config
 	stopOnce  sync.Once
+	startOnce sync.Once
 	stop      chan struct{}
 	stopFlag  int32
-	startFlag int32
 	opts      Options
 	tasks     chan *nsq.Message
 }
@@ -35,73 +36,64 @@ func NewWorker(opts ...Option) *Worker {
 		tasks: make(chan *nsq.Message),
 	}
 
-	cfg := nsq.NewConfig()
-	cfg.MaxInFlight = w.opts.maxInFlight
+	w.cfg = nsq.NewConfig()
+	w.cfg.MaxInFlight = w.opts.maxInFlight
 
-	if err := w.startProducer(cfg); err != nil {
-		panic(err)
-	}
-
-	if err := w.startConsumer(cfg); err != nil {
+	if err := w.startProducer(); err != nil {
 		panic(err)
 	}
 
 	return w
 }
 
-func (w *Worker) startProducer(cfg *nsq.Config) error {
+func (w *Worker) startProducer() error {
 	var err error
 
-	w.p, err = nsq.NewProducer(w.opts.addr, cfg)
+	w.p, err = nsq.NewProducer(w.opts.addr, w.cfg)
 
 	return err
 }
 
-func (w *Worker) startConsumer(cfg *nsq.Config) error {
-	if w.opts.disableConsumer {
-		return nil
-	}
-
-	var err error
-
-	w.q, err = nsq.NewConsumer(w.opts.topic, w.opts.channel, cfg)
-	if err != nil {
-		return err
-	}
-
-	w.q.AddHandler(nsq.HandlerFunc(func(msg *nsq.Message) error {
-		if len(msg.Body) == 0 {
-			// Returning nil will automatically send a FIN command to NSQ to mark the message as processed.
-			// In this case, a message with an empty body is simply ignored/discarded.
-			return nil
+func (w *Worker) startConsumer() (err error) {
+	w.startOnce.Do(func() {
+		w.q, err = nsq.NewConsumer(w.opts.topic, w.opts.channel, w.cfg)
+		if err != nil {
+			return
 		}
 
-	loop:
-		for {
-			select {
-			case w.tasks <- msg:
-				break loop
-			case <-w.stop:
-				if msg != nil {
-					// re-queue the job if worker has been shutdown.
-					msg.Requeue(-1)
-				}
-				break loop
-			case <-time.After(2 * time.Second):
-				msg.Touch()
+		w.q.AddHandler(nsq.HandlerFunc(func(msg *nsq.Message) error {
+			if len(msg.Body) == 0 {
+				// Returning nil will automatically send a FIN command to NSQ to mark the message as processed.
+				// In this case, a message with an empty body is simply ignored/discarded.
+				return nil
 			}
+
+		loop:
+			for {
+				select {
+				case w.tasks <- msg:
+					break loop
+				case <-w.stop:
+					if msg != nil {
+						// re-queue the job if worker has been shutdown.
+						msg.Requeue(-1)
+					}
+					break loop
+				case <-time.After(2 * time.Second):
+					msg.Touch()
+				}
+			}
+
+			return nil
+		}))
+
+		err = w.q.ConnectToNSQD(w.opts.addr)
+		if err != nil {
+			return
 		}
+	})
 
-		return nil
-	}))
-
-	err = w.q.ConnectToNSQD(w.opts.addr)
-	if err != nil {
-		return err
-	}
-
-	atomic.CompareAndSwapInt32(&w.startFlag, 0, 1)
-	return nil
+	return err
 }
 
 func (w *Worker) handle(job queue.Job) error {
@@ -172,12 +164,13 @@ func (w *Worker) Shutdown() error {
 		// notify shtdown event to worker and consumer
 		close(w.stop)
 		// stop producer and consumer
-		if atomic.LoadInt32(&w.startFlag) == 1 {
+		if w.q != nil {
 			w.q.ChangeMaxInFlight(0)
 			w.q.Stop()
 			<-w.q.StopChan
-			w.p.Stop()
 		}
+		w.p.Stop()
+
 		// close task channel
 		close(w.tasks)
 	})
@@ -195,6 +188,10 @@ func (w *Worker) Queue(job core.QueuedMessage) error {
 
 // Request fetch new task from queue
 func (w *Worker) Request() (core.QueuedMessage, error) {
+	if err := w.startConsumer(); err != nil {
+		return nil, err
+	}
+
 	clock := 0
 loop:
 	for {
